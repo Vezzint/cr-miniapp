4cfc48e223882c0810dc3be2c9cc2a1abaec9070
Vezzint <forcescircles@gmail.com>
2026-02-02 15:29:27 +0400

    Create config.ts

---
+++ backend/src/config.ts
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const BOT_TOKEN = process.env.BOT_TOKEN!;
+export const WEBAPP_URL = process.env.WEBAPP_URL!;
+export const BOT_SECRET = process.env.BOT_SECRET!;
